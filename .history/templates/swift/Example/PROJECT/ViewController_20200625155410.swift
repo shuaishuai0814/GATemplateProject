9e1523d07d2dd81bfe6572c91eefb2e158deb899
wangshuai331 <wangshuai331@jd.com>
2020-06-25 18:38:54 +0800

    moidfy

--- .history/templates/swift/Example/PROJECT/ViewController_20200625155410.swift
+++ .history/templates/swift/Example/PROJECT/ViewController_20200625155410.swift
@@ -3,7 +3,7 @@
 //  PROJECT
 //
 //  Created by PROJECT_OWNER on TODAYS_DATE.
-//  Copyright (c) TODAYS_YEAR ORGANIZATION_NAME. All rights reserved.
+//  Copyright (c) TODAYS_YEAR PROJECT_OWNER. All rights reserved.
 //
 
 import UIKit
